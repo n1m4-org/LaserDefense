4ab09d8fc1631d68df8e5b8e779aecbee7a39911
KazukiIhara <k023g0076@g.neec.ac.jp>
2026-09-09 14:08:26 +0900

    コンフリクト修正

--- Game/Scene/GimmickTuningScene.cpp
+++ Game/Scene/GimmickTuningScene.cpp
@@ -6,15 +6,15 @@
 #include "Math/MathUtils.hpp"
 #include "Time/Time.hpp"
 
-GimmickTuningScene::GimmickTuningScene()  = default;
+GimmickTuningScene::GimmickTuningScene() = default;
 GimmickTuningScene::~GimmickTuningScene() = default;
 
 void GimmickTuningScene::Initialize() {
-    constexpr Vector3 mainTowerPosition{0.0f, 0.0f, 0.0f};
+    constexpr Vector3 mainTowerPosition{ 0.0f, 0.0f, 0.0f };
 
     player_ = std::make_unique<Player>();
     player_->Initialize();
-    player_->SetPosition(mainTowerPosition + Vector3{0.0f, 0.0f, -8.0f});
+    player_->SetPosition(mainTowerPosition + Vector3{ 0.0f, 0.0f, -8.0f });
     player_->SetInput(input_);
 
     playerCamera_ = std::make_unique<PlayerCamera>();
@@ -26,15 +26,15 @@ void GimmickTuningScene::Initialize() {
 
     gimmickManager_ = std::make_unique<GimmickManager>();
     gimmickManager_->Initialize(GimmickContext{
-        player_.get(), towerManager_.get(), nullptr, Particle()});
+        player_.get(), towerManager_.get(), nullptr,nullptr, Particle() });
 
     floor_ = std::make_unique<Model>();
     floor_->Initialize("plane");
     floor_->SetTexture("white_x16.png");
-    floor_->SetColor({0.5f, 0.5f, 0.5f, 1.0f});
-    floor_->SetTranslate({0.0f, 0.0f, 0.0f});
-    floor_->SetRotate({-MathUtils::F_PI * 0.5f, 0.0f, 0.0f});
-    floor_->SetScale({40.0f, 40.0f, 1.0f});
+    floor_->SetColor({ 0.5f, 0.5f, 0.5f, 1.0f });
+    floor_->SetTranslate({ 0.0f, 0.0f, 0.0f });
+    floor_->SetRotate({ -MathUtils::F_PI * 0.5f, 0.0f, 0.0f });
+    floor_->SetScale({ 40.0f, 40.0f, 1.0f });
     floor_->Update();
 }
 
